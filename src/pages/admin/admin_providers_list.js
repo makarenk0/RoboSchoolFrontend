ddd77435be73b4f2944fd24463af9f7841dd8bab
makarenk0 <45842815+makarenk0@users.noreply.github.com>
2020-06-12 02:17:27 +0300

    removed useless dependencies

--- src/pages/admin/admin_providers_list.js
+++ src/pages/admin/admin_providers_list.js
@@ -11,11 +11,11 @@ export const Providers_list = () =>{
             <AddingForm params={[{type: 'text', title: 'provider name', servName: 'provider_name', servData: '',endRow: false}, 
                                   {type: 'phone', title: 'contact number', servName: 'contact_number', servData: '', endRow: true},
                                   {type: 'text', title: 'link', servName: 'site_link', servData: '', endRow: true},      
-                                 //{type: 'multiple-select', title: 'items', servName: 'adress', servData: 'https://localhost:44354/api/admin/get_all_schools', endRow: true}
-                                ]} submitRequest = 'https://localhost:44354/api/admin/add_provider'></AddingForm>
+                                 //{type: 'multiple-select', title: 'items', servName: 'adress', servData: 'https://roboschool-api.herokuapp.com/api/admin/get_all_schools', endRow: true}
+                                ]} submitRequest = 'https://roboschool-api.herokuapp.com/api/admin/add_provider'></AddingForm>
       </div>
         <div style={{marginTop: "50px"}}>
-            {<Table request={'https://localhost:44354/api/admin/get_all_providers'} onDelete={'https://localhost:44354/api/admin/delete_provider/'}></Table>}
+            {<Table request={'https://roboschool-api.herokuapp.com/api/admin/get_all_providers'} onDelete={'https://roboschool-api.herokuapp.com/api/admin/delete_provider/'}></Table>}
         </div>
       </div>
     )
